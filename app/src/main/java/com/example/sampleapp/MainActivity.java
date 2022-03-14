2147ec5c96d798a9fce5cbf94d57c9577dc3f9ca
Bilal_Ranjha12 <mbilal@leeddev.io>
2022-03-14 14:41:28 +0500

    First Commit

--- app/src/main/java/com/example/sampleapp/MainActivity.java
+++ app/src/main/java/com/example/sampleapp/MainActivity.java
@@ -23,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
+    // bilal
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
